05de6dcb4d1c5677ab4e97d1201c80b82cf60421
Araceli Teixeira <32455451+araceliteixeira@users.noreply.github.com>
2018-03-28 17:12:17 -0400

    Create note cell

---
+++ NotePro/View/NoteViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NoteViewCell.swift
+//  NotePro
+//
+//  Created by Araceli Teixeira on 28/03/18.
+//  Copyright © 2018 Orion Team. All rights reserved.
+//
+
+import UIKit
+
+class NoteViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
