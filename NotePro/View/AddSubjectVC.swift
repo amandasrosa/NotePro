36d3fd45e0477caa647dbe58d8b095535395817f
Araceli Teixeira <32455451+araceliteixeira@users.noreply.github.com>
2018-03-28 15:51:49 -0400

    Create Add Subject VC

---
+++ NotePro/View/AddSubjectVC.swift
@@ -0,0 +1,35 @@
+//
+//  AddSubjectVC.swift
+//  NotePro
+//
+//  Created by Araceli Teixeira on 28/03/18.
+//  Copyright © 2018 Orion Team. All rights reserved.
+//
+
+import UIKit
+
+class AddSubjectVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
