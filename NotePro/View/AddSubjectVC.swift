d5a3f603d88c7b9f3b633d9466823339f7824bdf
Araceli Teixeira <32455451+araceliteixeira@users.noreply.github.com>
2018-04-02 14:59:35 -0400

    Create Add Subject screen

--- NotePro/View/AddSubjectVC.swift
+++ NotePro/View/AddSubjectVC.swift
@@ -8,12 +8,17 @@
 
 import UIKit
 
-class AddSubjectVC: UIViewController {
-
+class AddSubjectVC: UIViewController, UITextFieldDelegate, HSBColorPickerDelegate {
+    @IBOutlet weak var subjectTextField: UITextField!
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var colorPicker: HSBColorPicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        subjectTextField.delegate = self
+        colorPicker.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +26,16 @@ class AddSubjectVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    // MARK: Delegates
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func HSBColorColorPickerTouched(sender: HSBColorPicker, color: UIColor, point: CGPoint, state: UIGestureRecognizerState) {
+        colorView.backgroundColor = color
+    }
 
     /*
     // MARK: - Navigation
