fabbcf3575dd4df4b969db41638bdadcf700c6e4
Denis Gois <denisdwtg@gmail.com>
2018-04-04 17:52:26 -0400

    New note layout, data and subject picker implementations

--- NotePro/View/NoteVC.swift
+++ NotePro/View/NoteVC.swift
@@ -9,18 +9,116 @@
 import UIKit
 
 class NoteVC: UITableViewController {
+    
+    @IBOutlet weak var titleField: UITextField!
+    @IBOutlet weak var descriptionField: UITextView!
+    @IBOutlet weak var subjectField: UITextField!
+    @IBOutlet weak var dateField: UITextField!
+    
+    
+    let datePickerView: UIDatePicker = UIDatePicker()
+    var subjectPickerView: SubjectPickerView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func handleSubjectPicker(_ sender: UITextField) {
+        createSubjectPicker(sender)
+    }
+    
+    fileprivate func createSubjectPicker(_ sender: UITextField) {
+        subjectPickerView = SubjectPickerView(subjectField: sender)
+        sender.inputView = subjectPickerView?.pickerView
+        subjectField.inputAccessoryView = createToolBarForSubject()
+    }
+    
+    @IBAction func handleDatePicker(_ sender: UITextField) {
+        createDataPicker(sender)
+    }
+    
+    fileprivate func createDataPicker(_ sender: UITextField) {
+        datePickerView.datePickerMode = .dateAndTime
+        datePickerView.endEditing(true)
+        datePickerView.isUserInteractionEnabled = true
+        sender.inputView = datePickerView
+        datePickerView.addTarget(self, action: #selector(NoteVC.hangleValueChange), for: .valueChanged)
+        dateField.inputAccessoryView = createToolBar()
+    }
+    
+    @objc func hangleValueChange() {
+        handleDataPicker()
+    }
+    
+    @objc func hangleValueChangeForSubject(subject: String) {
+        subjectField.text = subject
+    }
+    
+    fileprivate func createToolBar() -> UIToolbar {
+        let toolBar = UIToolbar()
+        toolBar.barStyle = .default
+        toolBar.isTranslucent = true
+        toolBar.tintColor = UIColor(red: 92/255, green: 216/255, blue: 255/255, alpha: 1)
+        toolBar.sizeToFit()
+        toolBar.setItems(createButtons(), animated: false)
+        toolBar.isUserInteractionEnabled = true
+        return toolBar
+    }
+    
+    fileprivate func handleDataPicker() {
+        let dateTimeFormatter = DateFormatter()
+        dateTimeFormatter.dateStyle = .medium
+        dateTimeFormatter.timeStyle = .short
+        dateField.text = dateTimeFormatter.string(from: datePickerView.date)
+    }
+    
+    fileprivate func createButtons() -> [UIBarButtonItem] {
+        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(NoteVC.doneClick))
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let cancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(NoteVC.cancelClick))
+        let buttons: [UIBarButtonItem] = [cancelButton, spaceButton, doneButton]
+        return buttons
+    }
+    
+    @objc func doneClick() {
+        handleDataPicker()
+        dateField.resignFirstResponder()
+    }
+    
+    @objc func cancelClick() {
+        dateField.resignFirstResponder()
+    }
+    
+    fileprivate func createToolBarForSubject() -> UIToolbar {
+        let toolBar = UIToolbar()
+        toolBar.barStyle = .default
+        toolBar.isTranslucent = true
+        toolBar.tintColor = UIColor(red: 92/255, green: 216/255, blue: 255/255, alpha: 1)
+        toolBar.sizeToFit()
+        toolBar.setItems(createButtonsForSubject(), animated: false)
+        toolBar.isUserInteractionEnabled = true
+        return toolBar
+    }
+    
+    fileprivate func createButtonsForSubject() -> [UIBarButtonItem] {
+        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(NoteVC.doneClickForSubject))
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let cancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(NoteVC.cancelClickForSubject))
+        let buttons: [UIBarButtonItem] = [cancelButton, spaceButton, doneButton]
+        return buttons
+    }
+    
+    @objc func doneClickForSubject() {
+        subjectField.resignFirstResponder()
+    }
+    
+    @objc func cancelClickForSubject() {
+        subjectField.resignFirstResponder()
+    }
 
     /*
     // MARK: - Navigation
@@ -32,4 +130,37 @@ class NoteVC: UITableViewController {
     }
     */
 
+}
+
+class SubjectPickerView: NSObject, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    var subjects: [String] = ["Travel", "Sports", "Home", "Work", "Study"]
+    var pickerView: UIPickerView = UIPickerView()
+    var subjectField: UITextField
+    
+    init(subjectField: UITextField) {
+        self.subjectField = subjectField
+        super.init()
+        self.pickerView.endEditing(true)
+        self.pickerView.isUserInteractionEnabled = true
+        self.pickerView.delegate = self
+        self.pickerView.dataSource = self
+        
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return subjects.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return subjects[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.subjectField.text = subjects[row]
+    }
 }
