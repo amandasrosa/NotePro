dcd13e5b7c7ba2642e94e64d63b3d7396a687cd4
Araceli Teixeira <32455451+araceliteixeira@users.noreply.github.com>
2018-04-14 10:20:28 -0400

    Insert image path in model

--- NotePro/Model/Picture.swift
+++ NotePro/Model/Picture.swift
@@ -13,15 +13,30 @@ public class Picture: Equatable {
     public private(set) var pictureId: Int?
     public private(set) var noteId: Int?
     public private(set) var picture: UIImage
+    public private(set) var path: String
     
     init(_ picture: UIImage) {
         self.picture = picture
+        self.path = ""
+    }
+    
+    init(_ path: String) {
+        self.picture = UIImage()
+        self.path = path
     }
     
     init(_ pictureId: Int, _ noteId: Int, _ picture: UIImage) {
         self.pictureId = pictureId
         self.noteId = noteId
         self.picture = picture
+        self.path = ""
+    }
+    
+    init(_ pictureId: Int, _ noteId: Int, _ path: String) {
+        self.pictureId = pictureId
+        self.noteId = noteId
+        self.picture = UIImage()
+        self.path = path
     }
     
     public static func ==(lhs: Picture, rhs: Picture) -> Bool {
